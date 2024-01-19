2dde1229a82622cd0702966e8662142fcda85daf
Chokkalingam-s <119471169+Chokkalingam-s@users.noreply.github.com>
2024-01-19 10:57:43 +0530

    Let's Deploy to chat

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Home from './pages/Home';
-import './style.scss';
-import { useContext } from 'react';
-import { BrowserRouter as Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
-import { AuthContext } from './context/AuthContext';
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import "./style.scss";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
-   const {currentUser} = useContext(AuthContext);
-   
-   const ProtectedRoute = ({children}) => {
-    if(!currentUser){
-      return <Navigate to="/login" />
+  const { currentUser } = useContext(AuthContext);
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
     }
-    return  children;
-   }
+
+    return children
+  };
+
   return (
     <BrowserRouter>
-       <Routes>
-           <Route path="/">
-                <Route index element={
-                  <ProtectedRoute>
-                  <Home />
-                </ProtectedRoute>} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-           </Route>
-       </Routes>
+      <Routes>
+        <Route path="/">
+          <Route
+            index
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-   
   );
 }
 
