286c20cb945ff15f1f6021cc0576e9e9d8ac93ac
Chokkalingam-s <119471169+Chokkalingam-s@users.noreply.github.com>
2024-01-22 10:04:19 +0530

    Added Search Box For MobileUi

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -83,6 +83,7 @@ const Search = () => {
           onChange={(e) => setUsername(e.target.value)}
           value={username}
         />
+        <button onClick={handleSearch}>Search</button>
       </div>
       {err && <span>User not found!</span>}
       {user && (
